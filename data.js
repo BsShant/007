a66805235963f1c0acf01065ab6a7249ab120fcc
BsShant <iambishant@gmail.com>
2020-11-23 22:43:41 +0545

    first repo

---
+++ data.js
@@ -0,0 +1,123 @@
+
+    records= {
+        studentsRecords:{
+            batch:{
+                2070: {
+                    active: 'yes',
+                    subjects:{
+                        math:{
+                            assignedTeachers:[ 'sanjay', 'hukum'],
+                            books: 'c maths',
+                        },
+                        science:{
+                            assignedTeachers:[ 'sanjeev', 'sanjay', 'nikhil'],
+                            books: 'applied science',
+                        },
+                        nepali:{
+                            assignedTeachers:[ 'teknath'],
+                            books: 'mero nepali',
+                        },
+                        english:{
+                            assignedTeachers:[ 'mark', 'harry'],
+                            books: 'c english',
+                        },
+                        social:{
+                            assignedTeachers:[ 'rajeev'],
+                            books: 'health&population',
+                        },
+                        account:{
+                            assignedTeachers:[ 'chiran'],
+                            books: 'accounts',
+                        },
+                        optionalMaths:{
+                            assignedTeachers:[ 'khim'],
+                            books: 'opt maths',
+                        }
+                    }
+                    ,
+                    students: [
+                        {
+                            registration: 2070001,
+                            Name: 'Bishant Pokharel',
+                            sex: 'male',
+                            age: '21',
+                            dob: 2056/04/30,
+                            bloodType: 'B+'
+                        },
+                        {
+                            registration: 2070002,
+                            Name: 'Bodhan Shrestha',
+                            sex: 'male',
+                            age: '22',
+                            dob: 2055/04/30,
+                            bloodType: 'A+'
+                        },
+                        {
+                            registration: 2070001,
+                            Name: 'Sanjay Dahal',
+                            sex: 'male',
+                            age: '22',
+                            dob: 2055/04/30,
+                            bloodType: 'B'
+                        }
+                    ]
+                }
+                    
+                
+                ,
+                2065:{
+                    active: 'no',
+                    students: [
+                        {
+                            registration: 2070001,
+                            Name: 'Nishant Pokharel',
+                            sex: 'male',
+                            age: '21',
+                            dob: 2056/04/30,
+                            bloodType: 'B+'
+                        },
+                        {
+                            registration: 2070002,
+                            Name: 'Pratik Shrestha',
+                            sex: 'male',
+                            age: '22',
+                            dob: 2055/04/30,
+                            bloodType: 'A+'
+                        },
+                        {
+                            registration: 2070001,
+                            Name: 'Manjay Dahal',
+                            sex: 'male',
+                            age: '22',
+                            dob: 2055/04/30,
+                            bloodType: 'B'
+                        }
+                    ]
+                }
+            }
+        },
+        teachersRecord:{
+            academic:[
+                {
+                    name: 'rajeev khadka',
+                    age:26,
+                    subject: 'social',
+                    fullTime:'yes',
+                    role:'admin'
+                },
+                {
+                    name: 'sunjay',
+                    age:26,
+                    subject: 'maths',
+                    fullTime:'yes',
+                    role:'teacher'
+                }
+            ]
+        },
+        admins:{
+
+        },
+        otherEmployees:{
+
+        }  
+    }
